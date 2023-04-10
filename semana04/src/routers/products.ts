ad93d2260b6e6d2d7b023301dfce7eede57736e5
Gabriel <gabrielja2antunes@gmail.com>
2023-04-10 17:00:37 -0300

    🚀 features: semana 05

--- semana04/src/routers/products.ts
+++ semana04/src/routers/products.ts
@@ -1,43 +1,46 @@
 import { Request, Response, Router } from "express";
-import productsService from "../services/products.service";
+import ProductService from "../services/products.service";
 
 const productsRouter: Router = Router();
 
-productsRouter.get('/', (req: Request, res: Response) => {
-  const products = productsService.getAll();
+productsRouter.get('/', async (req: Request, res: Response) => {
+  const products = await ProductService.getAll();
+  console.log(products);
+
   res.status(200).json(products)
+
 });
 
-productsRouter.get('/:id', (req: Request, res: Response) => {
-  const products = productsService.getById(Number(req.params.id));
+productsRouter.get('/:id', async (req: Request, res: Response) => {
+  const products = await ProductService.getBydocument(req.params.id);
   if (!products) return res.status(404).json({ message: "Product not found" })
   res.status(200).json(products)
 });
 
-productsRouter.post('/', (req: Request, res: Response) => {
+productsRouter.post('/', async (req: Request, res: Response) => {
   const newProduct = req.body;
   if (newProduct.quantity <= 0) {
     res.status(400).json({ message: "Quantidade do produto inválida" })
   }
 
-  productsService.create(newProduct)
+  await ProductService.create(newProduct)
   res.status(201).json({ message: "Produto cadastrado com sucesso!" });
 });
 
-productsRouter.put('/:id', (req: Request, res: Response) => {
+productsRouter.put('/:id', async (req: Request, res: Response) => {
   const { id } = req.params
   try {
-    productsService.update(Number(id), req.body)
+    await ProductService.update(id, req.body)
     res.status(200).json({ message: "Produto editado com sucesso!" })
   } catch (error: any) {
     res.status(400).json(error.message)
   }
 })
 
-productsRouter.delete('/remove/:id', (req: Request, res: Response) => {
+productsRouter.delete('/remove/:id', async (req: Request, res: Response) => {
   const { id } = req.params
   try {
-    productsService.remove(Number(id))
+    await ProductService.remove(id)
     res.status(200).json({ message: "Produto removido com sucesso!" });
   } catch (error: any) {
     res.status(400).json(error.message);
--- semana04/src/services/products.service.ts
+++ semana04/src/services/products.service.ts
@@ -1,37 +1,29 @@
-import { Products } from '../models/products.model';
-import { products } from "../utils/products";
+import ProductRepository from '../repositories/product.repository';
+import { Product } from '../models/products.model'
+
 
 export class ProductService {
-  allProducts: Products[] = products
-
   getAll() {
-    return this.allProducts;
+    const products = ProductRepository.getAll()
+    if (!products) throw new Error("Não existe nenhum produto cadastrado");
+    return products;
   }
 
-  getById(id: number) {
-    const product = this.allProducts.find((prod) => prod.id === id)
-    return product
+  getBydocument(document: string) {
+    return ProductRepository.getByDocument(document)
   }
 
-  create(document: Products) {
-    this.allProducts.push(document)
+  create(product: typeof Product) {
+    return ProductRepository.create(product)
   }
 
-  update(id: number, product: Products) {
-    const productToEditIndex = products.findIndex((prod) => prod.id === Number(id))
-
-    if (productToEditIndex === -1) {
-      throw new Error("Produto não encontrado.");
-    }
-    this.allProducts[productToEditIndex] = product
+  update(document: string, product: typeof Product) {
+    return ProductRepository.update(document, product)
   }
 
-  remove(id: number) {
-    const productToRemoveIndex = products.findIndex((prod) => prod.id === id)
-    if (productToRemoveIndex === -1) {
-      throw new Error("Estudante não encontrador");
-    }
-    this.allProducts.splice(productToRemoveIndex, 1)
+  remove(document: string) {
+
+    return ProductRepository.remove(document)
   }
 }
 
